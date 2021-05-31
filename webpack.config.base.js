c045809b5d6dcc1b634b3f90ed7f30a33f27b82d
sya-ri <sya79lua@gmail.com>
2021-05-31 10:41:32 +0900

    chore: Remove unused config

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -7,7 +7,6 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-    assetModuleFilename: 'images/[name][ext]',
   },
   target: ['web', 'es5'],
   resolve: {

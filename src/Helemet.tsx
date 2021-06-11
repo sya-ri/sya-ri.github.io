f59e1ef49a37d7c15302f5ae0519fec542ffdce8
sya-ri <sya79lua@gmail.com>
2021-06-11 11:15:46 +0900

    feat: Add ogp meta tag

--- src/Helemet.tsx
+++ src/Helemet.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Helmet as ReactHelmet } from 'react-helmet';
 import GeneralTheme from './GeneralTheme';
 
+const WebSiteURL = 'https://s7a.dev';
+const PageTitle = "sya-ri's Portfolio";
+
 const Helmet = (): JSX.Element => (
   <ReactHelmet>
     {/* Favicon https://www.favicon-generator.org/ */}
@@ -36,8 +39,20 @@ const Helmet = (): JSX.Element => (
       rel="stylesheet"
     />
 
+    {/* Open Graph Protocol */}
+    <meta property="og:url" content={WebSiteURL} />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content={PageTitle} />
+    <meta property="og:image" content={`${WebSiteURL}/icon-1200x630.png`} />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@fly_in_pig" />
+    <meta name="twitter:title" content={PageTitle} />
+    <meta name="twitter:image" content={`${WebSiteURL}/icon-1200x630.png`} />
+
     {/* General */}
-    <title>sya-ri</title>
+    <title>{PageTitle}</title>
     <meta charSet="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 

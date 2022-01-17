b0d78c3c89eb2ce253460125dd5a9042ab0c3b93
sya-ri <sya79lua@gmail.com>
2022-01-18 00:28:56 +0900

    feat: add index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,52 @@
-import { Text } from "@chakra-ui/react";
+import {
+  Button,
+  Center,
+  Image,
+  Link,
+  Text,
+  Icon,
+  Flex,
+} from "@chakra-ui/react";
+import { IconType } from "react-icons";
+import {
+  SiAmazon,
+  SiDiscord,
+  SiGithub,
+  SiInstagram,
+  SiTwitter,
+} from "react-icons/si";
 
-const Index = () => <Text>Hello World</Text>;
+const link = (icon: IconType, title: string, link: string) => ({
+  icon,
+  title,
+  link,
+});
+
+const links = [
+  link(SiTwitter, "Twitter", "https://link.s7a.dev/twitter"),
+  link(SiGithub, "GitHub", "https://link.s7a.dev/github"),
+  link(SiInstagram, "Instagram", "https://link.s7a.dev/instagram"),
+  link(SiDiscord, "Discord", "https://link.s7a.dev/discord"),
+  link(SiAmazon, "Amazon", "https://link.s7a.dev/amazon"),
+];
+
+const Index = () => (
+  <Center flexDirection="column" gap={4} minH="100vh" p="auto">
+    <Image alt="Icon" rounded="full" src="/icon.png" w={120} />
+    <Text fontSize={32} pb={4}>
+      sya_ri
+    </Text>
+    {links.map(({ icon, title, link }) => (
+      <Button key={title} w={200}>
+        <Link isExternal href={link}>
+          <Flex align="center">
+            <Icon as={icon} mr={2} />
+            {title}
+          </Flex>
+        </Link>
+      </Button>
+    ))}
+  </Center>
+);
 
 export default Index;

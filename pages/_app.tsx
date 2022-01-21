126fb94dee6b30aa1dafb5fffd2e84aaea22203b
sya-ri <sya79lua@gmail.com>
2022-01-21 21:54:56 +0900

    ui: change theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,23 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        backgroundColor: "gray.700",
+        color: "white",
+      },
+    },
+  },
+});
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <title>sya-ri</title>
       <meta content="sya-ri's Portfolio" property="description" />
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <Component {...pageProps} />
       </ChakraProvider>
     </>

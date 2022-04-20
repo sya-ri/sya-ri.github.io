adff2d92abdb4f6671033a8611f6c62c93502484
sya-ri <git@s7a.dev>
2022-04-20 23:00:36 +0900

    feat: Add /works close 67

---
+++ lib/Work.tsx
@@ -0,0 +1,72 @@
+import { IconProps } from "@chakra-ui/icon";
+import { Icon } from "@chakra-ui/react";
+import { CgBrowser } from "react-icons/cg";
+import { FiPackage, FiServer } from "react-icons/fi";
+import { GoTerminal } from "react-icons/go";
+import {
+  SiGithub,
+  SiKotlin,
+  SiNextdotjs,
+  SiReact,
+  SiRust,
+} from "react-icons/si";
+import MinecraftIcon from "./MinecraftIcon";
+
+export type Tag =
+  | "frontend"
+  | "backend"
+  | "cli"
+  | "library"
+  | "github-actions"
+  | "minecraft"
+  | "kotlin"
+  | "react"
+  | "nextjs"
+  | "rust";
+
+export const getTagIcon = (tag: Tag, props: IconProps): JSX.Element => {
+  switch (tag) {
+    case "frontend":
+      return <Icon as={CgBrowser} {...props} />;
+    case "backend":
+      return <Icon as={FiServer} {...props} />;
+    case "cli":
+      return <Icon as={GoTerminal} {...props} />;
+    case "library":
+      return <Icon as={FiPackage} {...props} />;
+    case "github-actions":
+      return <Icon as={SiGithub} {...props} />;
+    case "minecraft":
+      return <MinecraftIcon {...props} />;
+    case "kotlin":
+      return <Icon as={SiKotlin} {...props} />;
+    case "react":
+      return <Icon as={SiReact} {...props} />;
+    case "nextjs":
+      return <Icon as={SiNextdotjs} {...props} />;
+    case "rust":
+      return <Icon as={SiRust} {...props} />;
+  }
+};
+
+export interface Work {
+  title: string;
+  description: string;
+  tags: Tag[];
+  github?: [string, string];
+  youtube?: string;
+}
+
+export const work = (
+  title: string,
+  description: string,
+  tags: Tag[],
+  github?: [string, string],
+  youtube?: string
+): Work => ({
+  title,
+  description,
+  tags,
+  github,
+  youtube,
+});
---
+++ pages/works.tsx
@@ -0,0 +1,111 @@
+import {
+  Box,
+  Container,
+  Heading,
+  HStack,
+  Text,
+  Tag,
+  Icon,
+  Link,
+  Center,
+} from "@chakra-ui/react";
+import { SiGithub, SiYoutube } from "react-icons/si";
+import BackToTop from "../components/BackToTop";
+import { getTagIcon, work } from "../lib/Work";
+
+const works = [
+  work(
+    "PackSquash",
+    "Minecraftのリソースパックを圧縮するオープンソースプロジェクトです。コントリビューターとして携わり、現在はコアチームとして活動しています。",
+    ["cli", "github-actions", "minecraft", "rust"],
+    ["ComunidadAylas", "PackSquash"]
+  ),
+  work(
+    "SpigotEventList",
+    "Minecraftプラグイン開発において必要不可欠なイベントの一覧です。たくさんの人に使っていただいています。",
+    ["frontend", "minecraft", "nextjs"],
+    ["sya-ri", "spigot-event-list"]
+  ),
+  work(
+    "EasySpigotAPI",
+    "Minecraftプラグインのためのライブラリです。便利ですが少し煩雑になってしまいました。",
+    ["library", "minecraft", "kotlin"],
+    ["sya-ri", "EasySpigotAPI"]
+  ),
+  work(
+    "ktSpigot",
+    "EasySpigotAPIの問題点を解消したライブラリです。現在開発中です。",
+    ["library", "minecraft", "kotlin"],
+    ["sya-ri", "ktSpigot"]
+  ),
+  work(
+    "接触確認アプリ開発",
+    "コロナ禍で開催する文化祭のためにQRコードを使った接触確認アプリを1人で作りました。",
+    ["frontend", "backend", "kotlin", "react"],
+    ["sya-ri", "where-who-with"]
+  ),
+  work(
+    "KGit",
+    "JGitのKotlinラッパーライブラリです。初めてOSSライブラリを開発しました。",
+    ["library", "kotlin"],
+    ["sya-ri", "KGit"]
+  ),
+  work(
+    "プラグイン依頼制作",
+    "Minecraftの配信者や動画投稿者向けにプラグインの依頼制作をしています。",
+    ["minecraft", "kotlin"],
+    undefined,
+    "https://youtube.com/playlist?list=PLWJAXP1sPvvRSzctW4dYdrm9S_cpcIM96"
+  ),
+];
+
+const Works = () => {
+  return (
+    <Container py={[4, 8]}>
+      <Heading as="h1" textAlign="center">
+        Works
+      </Heading>
+      <Box py={4}>
+        {works.map(({ title, description, tags, github, youtube }) => {
+          return (
+            <Box key={title} py={2}>
+              <HStack alignItems="center" py={2}>
+                <Heading as="h2" fontSize={20}>
+                  {title}
+                </Heading>
+                {github && (
+                  <Link
+                    isExternal
+                    href={`https://github.com/${github.join("/")}`}
+                  >
+                    <Icon as={SiGithub} display="flex" fontSize={20} />
+                  </Link>
+                )}
+                {youtube && (
+                  <Link isExternal href={youtube}>
+                    <Icon as={SiYoutube} display="flex" fontSize={20} />
+                  </Link>
+                )}
+              </HStack>
+              <Box px={2}>
+                <HStack>
+                  {tags.map((tag) => (
+                    <Tag key={tag}>
+                      #{tag} {getTagIcon(tag, { ml: 1 })}
+                    </Tag>
+                  ))}
+                </HStack>
+                <Text py={2}>{description}</Text>
+              </Box>
+            </Box>
+          );
+        })}
+      </Box>
+      <Center>
+        <BackToTop />
+      </Center>
+    </Container>
+  );
+};
+
+export default Works;
